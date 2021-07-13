622604766768220e18664cf4fc65dca7b52eb991
Vibhanshu Biswas <vibhanshu@neurosensum.com>
2021-07-13 13:42:24 +0530

    individual packages

--- common_utilities.py
+++ common_utilities.py
@@ -1,4 +1,4 @@
 from subprocess import check_call, CalledProcessError
 import os
 
-check_call(['apt-get', 'install', '-y', 'zip unzip glances htop'], stdout=open(os.devnull,'wb'))
+check_call(['apt-get', 'install', '-y', 'zip', 'unzip', 'glances', 'htop'], stdout=open(os.devnull,'wb'))
